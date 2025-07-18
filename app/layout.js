37a2357ebe19ea4df9b665d4c926fd15a4cfc036
googoocone1 <googoocone1@naver.com>
2025-07-18 11:22:55 +0900

    구글 태그 추가

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Noto_Sans_KR } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/footer";
+import Script from "next/script"; // Script 컴포넌트를 import 합니다.
 
 const notoSansKR = Noto_Sans_KR({
   subsets: ["latin"],
@@ -14,12 +15,38 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  // GTM ID를 변수로 관리하면 편리합니다.
+  const gtmId = "GTM-M9KFKK4K";
+
   return (
     <html lang="en">
       <body className={`${notoSansKR.className} antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <Header></Header>
         {children}
         <Footer></Footer>
+
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${gtmId}');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
       </body>
     </html>
   );
