fea18a82ebcd4ef75a4813ce5e1a9cad0546daac
googoocone1 <googoocone1@naver.com>
2025-10-15 18:08:22 +0900

    영상 업로드 완료

--- components/Section1.jsx
+++ components/Section1.jsx
@@ -11,12 +11,8 @@ const Section1 = () => {
         preload="auto"
         poster="/preview.png"
       >
-        <source
-          src="/main_mv.mp4"
-          type="video/mp4"
-          media="(max-width: 768px)"
-        />
-        <source src="/main_mv.mp4" type="video/mp4" />
+        <source src="/mainmv.mp4" type="video/mp4" media="(max-width: 768px)" />
+        <source src="/mainmv.mp4" type="video/mp4" />
         브라우저가 비디오를 지원하지 않습니다.
       </video>
     </section>
